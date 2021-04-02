d9ca505fcc044c6df14b27074bb8b22ff8d91832
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-04-03 02:50:24 +0300

    Добавлен пример определения контейнеров стандартных типов stl.

---
+++ is_stl_container/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "is_stl_container.h"
+
+int main()
+{
+    std::cout << std::boolalpha;
+    std::cout << is_stl_container<std::vector<int>>::value << std::endl;
+    std::cout << is_stl_container<std::vector<int> const&>::value << std::endl;
+    std::cout << is_stl_container<int>::value << std::endl;
+}
