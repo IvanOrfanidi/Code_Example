dd7a389279212514a8dcf2a82092396cf483d684
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-09-19 21:58:24 +0300

    Переделал измерения под микросекунды.

--- duration_of_time/main.cpp
+++ duration_of_time/main.cpp
@@ -14,22 +14,24 @@ T generate_data(size_t size)
     static std::default_random_engine generator;
 
     T data(size);
-    std::generate(data.begin(), data.end(), []() { return distribution(generator); });
+    std::generate(data.begin(), data.end(), []() {
+        return distribution(generator);
+    });
     return data;
 }
 
 volatile int sink;
 int main()
 {
-    const std::chrono::milliseconds start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+    const std::chrono::microseconds start = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch());
 
-    std::vector<int> vec = generate_data<std::vector<int>>(100'000);
+    std::vector<int> vec = generate_data<std::vector<int>>(1'000);
     std::sort(vec.begin(), vec.end());
 
-    std::chrono::milliseconds end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+    std::chrono::microseconds end = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch());
 
-    const std::chrono::milliseconds diff = end - start;
-    std::cout << "Time to fill and sort a vector of " << vec.size() << " ints : " << diff.count() << " ms\n";
+    const auto diff = end - start;
+    std::cout << "Time to fill and sort a vector of " << vec.size() << " ints : " << diff.count() << " us\n";
 
     return EXIT_SUCCESS;
 }
